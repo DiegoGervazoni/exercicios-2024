5c936872e57847caa4cc4591cb996c5ca47bd92c
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2024-05-03 15:35:23 -0300

    (WIP) adicionando a criacao do `criar topico` no html, no scss e logica para interacao do botao `criar topico` e `enviar`

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -1,18 +1,36 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
-export class AppComponent{
-
+export class AppComponent {
   show: boolean = true;
 
   showMore(): void {
     this.show = !this.show;
   }
 
-}
+  showForm: boolean = false;
+  topicSent: boolean = false;
+  topicTitle = '';
+  topicContent = '';
+  topicMessage = ''; // Nova variável adicionada
 
+  createTopic(): void {
+    this.showForm = true;
+    this.topicSent = false;
+  }
+
+  sendTopic(): void {
+    if (!this.topicTitle.trim() || !this.topicContent.trim()) {
+      alert('Por favor, preencha tanto o título quanto o conteúdo do tópico.');
+      return;
+    }
+    
+    this.topicMessage = `Título do tópico: ${this.topicTitle}\nConteúdo do tópico: ${this.topicContent}`; 
+    this.showForm = true;
+    this.topicSent = true;
+  }
+}
